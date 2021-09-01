c226cfb9e2484c015929cecac5167b79678391f7
Vitaris <53018032+Vitaris@users.noreply.github.com>
2021-09-01 13:18:20 +0200

    Added gpu fan

--- gpu_fan.py
+++ gpu_fan.py
@@ -1,8 +1,4 @@
 from machine import Pin, PWM
-import machine
-import time
-import random
-from lcd import *
 
 # Led
 led = PWM(Pin(25))
@@ -24,17 +20,6 @@ reset = Pin(13, Pin.IN, Pin.PULL_DOWN)
 PWM_Fan = PWM(Pin(12))
 PWM_Fan.freq(25000)
 
-# LCD Object and pin definition
-LCD = lcd(RS=17, RW=1, EN=16, D4=18, D5=19, D6=20, D7=21, COL=20, ROW=4)
-
-# LCD Initializing
-LCD.init()
-
-# LCD Clear
-LCD.clrscr()
-
-# Write down label
-LCD.pos_puts(0, 0, "Fan Speed:")
 speed = 3930
 
 prev_val = True    
